13cf9860e922e0eade1004f2e9066b19d78cd18d
Aibu1at <aibulat6@gmail.com>
2025-07-21 19:58:48 +0500

    Добавил разные варианты написания параметров

--- get_measures_from_patent/llm_patent_agents/config.py
+++ get_measures_from_patent/llm_patent_agents/config.py
@@ -13,7 +13,7 @@ API_RETRY_ATTEMPTS = 3
 API_RETRY_DELAY = 1  # seconds
 
 # --- Chunking and Pre-filtering Configuration ---
-METRIC_REGEX_PATTERN = r'\b(IC50|Ki|EC50|Kd|inhibition|binding assay)\b'
+METRIC_REGEX_PATTERN = r'\b(IC[\s_-]*50|IC₅₀|Ki|Kd|EC[\s_-]*50|EC₅₀|pIC[\s_-]*50|pIC₅₀|pEC[\s_-]*50|pEC₅₀|pKi|pKd|logIC50|log[\s]*\([\s]*IC50[\s]*\)|-log[\s]*\([\s]*IC50[\s]*\)|logKi|log[\s]*\([\s]*Ki[\s]*\)|-log[\s]*\([\s]*Ki[\s]*\)|logKd|log[\s]*\([\s]*Kd[\s]*\)|-log[\s]*\([\s]*Kd[\s]*\)|logEC50|log[\s]*\([\s]*EC50[\s]*\)|-log[\s]*\([\s]*EC50[\s]*\)|inhibition[\s-]*constant|dissociation[\s-]*constant|binding[\s-]*constant|half[\s-]*maximal[\s-]*inhibitory[\s-]*concentration|half[\s-]*maximal[\s-]*effective[\s-]*concentration|binding[\s-]*assay)\b'
 NEGATIVE_KEYWORDS_REGEX = r'\b(cytotoxicity|cell viability|cell growth|platelet count|viability assay|cytotoxic)\b'
 CHUNK_CONTEXT_SIZE = 1500
 CHUNK_OVERLAP = 300
